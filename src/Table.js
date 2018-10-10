1027a08547efc9dcf7702ccb76982857a2745de7
berramou <a.berramou@void.fr>
2018-10-10 22:27:27 +0100

    separate table component

--- src/Table.js
+++ src/Table.js
@@ -1,29 +1,54 @@
 import React, {Component} from 'react';
 
+/**
+ *  TableHeader Simple Component
+ * @returns {*}
+ * @constructor
+ */
+const TableHeader = () => {
+    return (
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Job</th>
+        </tr>
+        </thead>
+    );
+};
+
+/**
+ * TableBody Simple Component
+ * @returns {*}
+ * @constructor
+ */
+const TableBody = () => {
+    return (
+        <tbody>
+        <tr>
+            <td>MArk zekerbug</td>
+            <td>FaceBook Founder</td>
+        </tr>
+        <tr>
+            <td>Steve Jobs</td>
+            <td>Apple Founder</td>
+        </tr>
+        <tr>
+            <td>Steve Jobs</td>
+            <td>Apple Founder</td>
+        </tr>
+        </tbody>
+    );
+};
+
+/**
+ *  Table class component
+ */
 class Table extends Component {
     render() {
         return (
             <table>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Job</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>MArk zekerbug</td>
-                    <td>FaceBook Founder</td>
-                </tr>
-                <tr>
-                    <td>Steve Jobs</td>
-                    <td>Apple Founder</td>
-                </tr>
-                <tr>
-                    <td>Steve Jobs</td>
-                    <td>Apple Founder</td>
-                </tr>
-                </tbody>
+                <TableHeader/>
+                <TableBody/>
             </table>
         );
     }
